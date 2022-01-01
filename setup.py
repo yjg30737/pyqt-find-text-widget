3906e52e32e3fa777673781f7413fa339da68b24
Jung Gyu Yoon <yjg30737@gmail.com>
2022-01-01 14:18:20 +0900

    Add mistakenly omitted icon file(word.png) in setup.py, Update version (0.1.0 -> 0.1.1)

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyqt-find-text-widget',
-    version='0.1.0',
+    version='0.1.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
     package_data={'pyqt_find_text_widget.style': ['button.css'],
-                  'pyqt_find_text_widget.ico': ['prev.png', 'next.png', 'case.png', 'regex.png', 'close.png']},
+                  'pyqt_find_text_widget.ico': ['prev.png', 'next.png', 'case.png', 'word.png',
+                                                'regex.png', 'close.png']},
     description='PyQt5 find text widget',
     url='https://github.com/yjg30737/pyqt-find-text-widget.git',
     install_requires=[

bdc800b99e8a00e6128b2b3b541824810ab92c8b
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-20 18:04:31 +0900

    Update version (0.3.2 -> 0.4.0)

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyqt-find-text-widget',
-    version='0.3.2',
+    version='0.4.0',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
     package_data={'pyqt_find_text_widget.ico': ['prev.svg', 'next.svg', 'case.svg', 'word.svg',
                                                 'regex.svg', 'close.svg']},
-    description='PyQt5 find text widget',
+    description='PyQt widget which is able to find text in QTextEdit/QTextBrowser',
     url='https://github.com/yjg30737/pyqt-find-text-widget.git',
     install_requires=[
         'PyQt5>=5.8',

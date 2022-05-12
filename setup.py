f6a0597abf3cb5bbb52624f0bbe59e392d425592
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-13 08:30:04 +0900

    Update package's url, update setup.py to upload this to package

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-find-text-widget',
-    version='0.4.0',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
@@ -11,8 +18,10 @@ setup(
                                                 'regex.svg', 'close.svg']},
     description='PyQt widget which is able to find text in QTextEdit/QTextBrowser',
     url='https://github.com/yjg30737/pyqt-find-text-widget.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
-        'pyqt-svg-icon-pushbutton @ git+https://git@github.com/yjg30737/pyqt-svg-icon-pushbutton.git@main'
+        'pyqt-svg-icon-pushbutton>=0.0.1'
     ]
 )
